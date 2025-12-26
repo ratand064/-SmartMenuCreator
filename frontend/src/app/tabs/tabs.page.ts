ec3d1ef87fb362e0bbce75d6d15fb2c4cd4b4950
Your Name <your-email@gmail.com>
2025-12-26 21:10:20 +0530

    deployment

--- frontend/src/app/tabs/tabs.page.ts
+++ frontend/src/app/tabs/tabs.page.ts
@@ -1,9 +1,10 @@
 import { Component, EnvironmentInjector, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { addCircle, restaurant, cart } from 'ionicons/icons';
 import { CartService } from '../services/cart.service';
+import { ApiService } from '../services/api.service'; // Import ApiService
 
 @Component({
   selector: 'app-tabs',
@@ -14,15 +15,38 @@ import { CartService } from '../services/cart.service';
 })
 export class TabsPage implements OnInit {
   public environmentInjector = inject(EnvironmentInjector);
-  
   isAnimating: boolean = false;
-  constructor(public cartService: CartService) {
+
+  constructor(
+    public cartService: CartService,
+    private api: ApiService // ApiService inject karein
+  ) {
     addIcons({ addCircle, restaurant, cart });
   }
 
   ngOnInit() {
+    // 1. App start hote hi Cart ka latest data fetch karein
+    this.refreshCart();
+
+    // 2. Animation listener
     this.cartService.cartAnimation$.subscribe(() => {
       this.triggerBumpEffect();
+    });
+  }
+
+  // Jab bhi user Tabs page par wapas aaye, cart check karein
+  ionViewWillEnter() {
+    this.refreshCart();
+  }
+
+  refreshCart() {
+    this.api.getCart().subscribe({
+      next: (res: any) => {
+        if (res.success && res.data && res.data.items) {
+          // Service me count update karein taaki badge show ho
+          this.cartService.updateCartCount(res.data.items.length);
+        }
+      }
     });
   }
 
